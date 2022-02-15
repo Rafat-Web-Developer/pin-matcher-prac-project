9ad64bac1a28c28325a9a04d9aac575c242e921e
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-02-15 13:12:05 +0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -13,3 +13,27 @@ function generatePin(){
     const pin = createPin();
     document.getElementById('showRandomPin').value = pin;
 }
+
+document.getElementById('key').addEventListener('click', function(event){
+    const currentKey = document.getElementById('showKey');
+    if(event.target.innerText == 'C'){
+        currentKey.value = '';
+    }else if(currentKey.value != ''){
+        currentKey.value = currentKey.value + event.target.innerText;
+    }else{
+        currentKey.value = event.target.innerText;
+    }
+});
+function matchPin(){
+    const randomField = document.getElementById('showRandomPin');
+    const keyField = document.getElementById('showKey');
+    const randomNumber = parseInt(randomField.value);
+    const keyNumber = parseInt(keyField.value);
+    if(randomNumber == keyNumber){
+        document.getElementById('match').style.display = 'block';
+        document.getElementById('notMatch').style.display = 'none';
+    }else{
+        document.getElementById('notMatch').style.display = 'block';
+        document.getElementById('match').style.display = 'none';
+    }
+}
